ee4322f76259846c0cdb739516d590560de57224
Ginger-Mano <mcasimirian@gmail.com>
2021-01-05 19:19:18 -0500

    state addition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <div className="title">
-        <h2 className="title1">manon sainton</h2>
-      </div>
-    </div>
-  );
+class App extends Component {
+  state = {
+    button: false,
+    backgroundChange: false
+  }
+
+  startBClicked = () => {
+    this.setState({
+      button: !this.state.button,
+      backgroundChange: !this.state.backgroundChange
+    })
+  }
+  render() {
+    return (
+      this.state.backgroundChange ?
+
+        <div className="Appchange">
+          <div className="title">
+            <h2 className="title1">manon sainton</h2>
+          </div>
+          {this.state.button ? <img className="img" alt="" src="https://c0.wallpaperflare.com/preview/66/740/843/programmer-computer-woman-support.jpg" /> : null}
+
+          {this.state.button ? <img className="imgtop" alt="" src="https://i.dlpng.com/static/png/6759622_preview.png" /> : null}
+
+          {this.state.button ? <img className="imgright" alt="" src="https://library.kissclipart.com/20180908/toe/kissclipart-clip-art-notebook-clipart-notebook-clip-art-373d6e0403ed6cf2.png" /> : null}
+
+          <div className="start">
+            <button className="startB" onClick={this.startBClicked}>Start <br></br> here</button>
+          </div>
+        </div> :
+
+        <div className="App">
+          <div className="title">
+            <h2 className="title1">manon sainton</h2>
+          </div>
+          {this.state.button ? <img className="img" alt="" src="https://c0.wallpaperflare.com/preview/66/740/843/programmer-computer-woman-support.jpg" /> : null}
+
+          {this.state.button ? <img className="imgtop" alt="" src="https://i.dlpng.com/static/png/6759622_preview.png" /> : null}
+
+          {this.state.button ? <img className="imgright" alt="" src="https://library.kissclipart.com/20180908/toe/kissclipart-clip-art-notebook-clipart-notebook-clip-art-373d6e0403ed6cf2.png" /> : null}
+
+          <div className="start">
+            <button className="startB" onClick={this.startBClicked}>Start <br></br> here</button>
+          </div>
+        </div>
+    )
+  }
 }
 
 export default App;
