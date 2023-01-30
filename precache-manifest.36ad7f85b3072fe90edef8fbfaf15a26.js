2305d5d726208004fde4b0ced22940d31b0e9b7d
Manon Sainton <manonsainton.dev@gmail.com>
2023-01-30 16:54:59 -0500

    Updates

---
+++ precache-manifest.36ad7f85b3072fe90edef8fbfaf15a26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c318f03c2148d33fc111",
+    "url": "/react-gh-pages/static/css/main.1105b2e2.chunk.css"
+  },
+  {
+    "revision": "c318f03c2148d33fc111",
+    "url": "/react-gh-pages/static/js/main.9ad4bd3d.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "3317aeaf8b758f72f541",
+    "url": "/react-gh-pages/static/js/2.f690817b.chunk.js"
+  },
+  {
+    "revision": "08071f39f4eb5f201776d297f16dd75d",
+    "url": "/react-gh-pages/static/media/186669__fordps3__computer-boop.08071f39.wav"
+  },
+  {
+    "revision": "56a1b8364d1584aba4de91c2eca81343",
+    "url": "/react-gh-pages/static/media/IMG-2095.56a1b836.jpg"
+  },
+  {
+    "revision": "5099124fbbbbad13c14a93f68852e7ae",
+    "url": "/react-gh-pages/index.html"
+  }
+];
