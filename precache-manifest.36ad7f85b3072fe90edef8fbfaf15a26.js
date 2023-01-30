064866a7ab9cb9b1ca9eaaaa161fe4a8d3faa341
Manon Sainton <manonsainton.dev@gmail.com>
2023-01-30 18:18:39 -0500

    Updates

--- precache-manifest.36ad7f85b3072fe90edef8fbfaf15a26.js
+++ precache-manifest.36ad7f85b3072fe90edef8fbfaf15a26.js
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/react-gh-pages/static/media/IMG-2095.56a1b836.jpg"
   },
   {
-    "revision": "09ef9a31bd6d7720cbeaf2eab8108143",
+    "revision": "5099124fbbbbad13c14a93f68852e7ae",
     "url": "/react-gh-pages/index.html"
   }
 ];
